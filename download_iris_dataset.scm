c3ae43c7341ef6ae475def88daae39376a2dffb3
hi-watana <hero95alive@gmail.com>
2020-02-17 17:16:14 +0900

    Deleted.

--- download_iris_dataset.scm
+++
@@ -1,8 +0,0 @@
-(use rfc.http)
-(use text.csv)
-
-(define (main args)
-  (receive (status header body)
-           (http-get "raw.githubusercontent.com" "/pandas-dev/pandas/master/pandas/tests/data/iris.csv")
-           (with-output-to-file "iris.csv"
-                              (lambda () (display body)))) 0)
